f7f5f96c88e2e7ad0e78fd06ae20b2c53c266f14
garima230306 <singh.garima809811@gmail.com>
2026-07-21 20:14:39 +0530

    update

---
+++ access.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=11;
+    int arr[11]={2,5,7,9};
+    cout<<"find the address of array"<<endl;
+    for(int i=0;i<n;i++){
+    cout<<arr[i];
+    }
+}
---
+++ index.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a[6]={2,7,8,6,3};
+cout<<"find address of array"<<endl;
+cout<<a[2]<<endl;
+return 0;
+}
